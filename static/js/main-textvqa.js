fe58804a08aeee2504b53dc543f4151631a47fa5
Ayush Shrivastava <shrivastava.ayush1996@gmail.com>
2019-03-07 17:31:29 -0500

    Added TextVQA

---
+++ static/js/main-textvqa.js
@@ -0,0 +1,7 @@
+jQuery(document).ready(function($) {
+    var timezone = "Etc/GMT" ;
+    var deadline = moment.tz("2019-05-18 23:59:59", timezone);
+    $('#countdown-textvqa').countdown(deadline.toDate(), function(event) {
+        $(this).html(event.strftime('%D days %Hh %Mm %Ss'));
+    });
+});
