91fb2d1d5722b3f59aa6796a4bbea47bf96bc1bb
Ayush Shrivastava <shrivastava.ayush1996@gmail.com>
2019-05-14 21:15:17 -0400

    Extended TextVQA deadline

--- static/js/main-textvqa.js
+++ static/js/main-textvqa.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function($) {
     var timezone = "Etc/GMT" ;
-    var deadline = moment.tz("2019-05-18 23:59:59", timezone);
+    var deadline = moment.tz("2019-05-27 23:59:59", timezone);
     $('#countdown-textvqa').countdown(deadline.toDate(), function(event) {
         $(this).html(event.strftime('%D days %Hh %Mm %Ss'));
     });
