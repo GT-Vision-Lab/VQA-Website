f4a3246f798af38119775a04e9a103312f774985
Ayush Shrivastava <shrivastava.ayush1996@gmail.com>
2020-02-08 22:00:22 -0500

    Added VQA and VisDial challenge links

--- static/js/main-visdial.js
+++ static/js/main-visdial.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function($) {
     var timezone = "Etc/GMT" ;
-    var deadline = moment.tz("2019-05-18 23:59:59", timezone);
+    var deadline = moment.tz("2020-05-21 23:59:59", timezone);
     $('#countdown-visdial').countdown(deadline.toDate(), function(event) {
         $(this).html(event.strftime('%D days %Hh %Mm %Ss'));
     });
