a3fd1e2f42b8cb38a38cf8a72f3b23084d194975
Ayush Shrivastava <shrivastava.ayush1996@gmail.com>
2021-03-07 21:17:42 -0500

    Updated counter for VQA deadline

--- static/js/main-vqa.js
+++ static/js/main-vqa.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function($) {
     var timezone = "Etc/GMT" ;
-    var deadline = moment.tz("2020-05-15 23:59:59", timezone);
+    var deadline = moment.tz("2021-05-07 23:59:59", timezone);
     $('#countdown-vqa').countdown(deadline.toDate(), function(event) {
         $(this).html(event.strftime('%D days %Hh %Mm %Ss'));
     });
